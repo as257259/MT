cbd0f079f5018495fc79981a8783d98a3f6a1216
klaas8 <132535742+klaas8@users.noreply.github.com>
2026-01-01 14:07:34 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ MAX_RETRY = 3
 Retry = {}
 accounts_list = {}
 s_list = {}
-prefs.put("theme", "dark")
+print(prefs.get("theme", "失败"))
 """
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
